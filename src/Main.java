3200e602f40ed1cb3c47e5b5e5d5c9c1d789877a
Victor Ungureanu <victoru89@gmail.com>
2012-09-30 14:53:53 +0200

    Merged gitignore

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         // TODO: Dynamic statistics for each query.
     }
 
-    public void main(String args[]) {
+    public static void main(String args[]) {
 
         if (args.length < 1) {
             System.out.println("Usage: Main <document_folder>");
