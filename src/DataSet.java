765f1e5baff91b570132a64a5df82d359e04f6ab
Victor Carbune <vcarbune@adobe.com>
2012-09-30 15:14:35 +0300

    added the simple data structure index

--- src/DataSet.java
+++ src/DataSet.java
@@ -1,14 +1,35 @@
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class DataSet {
-	private HashMap<String, LinkedList<String>> data;
+	private HashMap<String, TreeSet<String>> data = new HashMap<String, TreeSet<String>>();
 
-	public LinkedList<String> getDocIdList(String term) {
+	/**
+	 * Returns the list of documents ids in which a specific term appears.
+	 * 
+	 * @param term The term for which to return the doc id list.
+	 * @return The list of document ids.
+	 */
+	public TreeSet<String> getDocIdList(String term) {
 		return data.get(term);
 	}
 	
+	/**
+	 * Marks the appearance of a term in a docId.
+	 * 
+	 * @param term The term discovered.
+	 * @param docId The document id in which it appears.
+	 */
 	public void addPair(String term, String docId) {
-		// TODO():
+		TreeSet<String> docIdList = data.get(term);
+
+		if (docIdList == null) {
+			docIdList = new TreeSet<String>();
+			data.put(term, docIdList);
+		}
+		
+		if (!docIdList.contains(docId)) {
+			docIdList.add(docId);
+		}
 	}
 }
