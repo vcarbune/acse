8cd0630e131488d265e74cd23910a993aaa976df
lvamanu <loredanavamanu@gmail.com>
2012-10-08 17:15:08 +0200

    Added javadoc for methods.

--- src/Crawler.java
+++ src/Crawler.java
@@ -40,7 +40,7 @@ public class Crawler {
     public void readStopWords() {
         this.stopWordsFlag = true;
         stopWords = new HashSet<String>();
-        
+
         FileInputStream inputStream;
         try {
             inputStream = new FileInputStream(stopWordsFile);
@@ -67,12 +67,9 @@ public class Crawler {
      * were deleted. It adds the term plus the document Id and the position of
      * the word in the file into the dataSet.
      * 
-     * @param reader
-     *            The BufferedReader used to read from file.
-     * @param docID
-     *            The name of the file.
-     * @throws IOException
-     *             Exception thrown in case the file can not be read
+     * @param reader The BufferedReader used to read from file.
+     * @param docID The name of the file.
+     * @throws IOException Exception thrown in case the file can not be read
      */
     private void processFileStopWordOn(BufferedReader reader, String docID)
     throws IOException {
@@ -87,7 +84,7 @@ public class Crawler {
                 if (stopWords.contains(token) == false) {
                     countPos++;
                     String formatted = token.replaceAll("[^a-zA-Z0-9]", "");
-                  //  System.out.println(formatted + " " + docID);
+                    //  System.out.println(formatted + " " + docID);
                     if (formatted.isEmpty() == false) {
                         dataSet.addPair(formatted.toUpperCase(), docID,
                                 countPos);
@@ -96,6 +93,14 @@ public class Crawler {
             }
         }
     }
+
+    /**
+     * Process reading from a corpus file in case no flag is set. 
+     * 
+     * @param reader The BufferedReader object that is connected to a corpus file
+     * @param docID  The ID of the document currently reading from.
+     * @throws IOException Exception thrown in case the file can not be read.
+     */
 
     private void processLine(BufferedReader reader, String docID)
     throws IOException {
@@ -114,6 +119,16 @@ public class Crawler {
         }
     }
 
+    /**
+     * Reads the corpus documents line by line and adds the terms together with 
+     * the document ID and the word position into a DataSet. The terms are changed 
+     * accordingly to which flags are on. If the stopWords flag is set then the stop 
+     * words are removed. If the stemming is on then each term is stemmed and then 
+     * added to the DataSet.
+     *  
+     * @return  A DataSet object containing terms from corpus.
+     * @throws IOException Exception thrown in case the corpus file can not be found.
+     */
     public DataSet readDocuments() throws IOException {
         File folder = new File(folderName);
         File[] listOfFiles = folder.listFiles();
@@ -144,18 +159,32 @@ public class Crawler {
         return dataSet;
     }
 
+    /**
+     * Retrieves all the names of documents from corpus.
+     * @return A treeSet object containing all documents name. 
+     */
     public TreeSet<String> getDocumentIDs() {
         return documents;
     }
 
+    /**
+     * Retrieves the stop words read from the stopWordsFile
+     * but upper cased.
+     * @return A HashSet containing all stop words.
+     */
     public HashSet<String> getStopWords() {
         return stopWords;
     }
+    
 
+    /**
+     * Sets the file where the stop words are stored. 
+     * @param stopWordsFile The name of the file.
+     */
     public void setStopWordsFile(String stopWordsFile) {
         this.stopWordsFile = stopWordsFile;
     }
-    
-    
+
+
 
 }
