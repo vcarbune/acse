3649dfc88cb0fbc66b6debcf51e5bed35fee96c1
lvamanu <loredanavamanu@gmail.com>
2012-10-09 22:38:42 +0200

    Added Proximity search.

--- src/QueryHandler.java
+++ src/QueryHandler.java
@@ -45,18 +45,20 @@ public class QueryHandler {
      * @return a Map from DocIds to another map of terms to positions. 
      */
     private HashMap<String, HashMap<String, TreeSet<Integer>>>
-            getDocIdsMatchingPhrase(Query query) {
+    getDocIdsMatchingPhrase(Query query) {
         ArrayList<String> terms = query.getTerms();
         HashMap<String, HashMap<String, TreeSet<Integer>>> documents =
-                new HashMap<String, HashMap<String, TreeSet<Integer>>>();
+            new HashMap<String, HashMap<String, TreeSet<Integer>>>();
 
         for(String term : terms) {
             TreeSet<DocIdEntry> docIDs = dataSet.getDocIdEntrySet(term);
+            if(docIDs == null)
+                return  documents;
             Iterator<DocIdEntry> iterator = docIDs.iterator();
 
             while(iterator.hasNext()){
                 DocIdEntry currEntry = iterator.next();
-                
+
                 HashMap<String, TreeSet<Integer>> termToPos = documents.get(currEntry.getDocId());
                 if(termToPos == null) {
                     termToPos = new HashMap<String, TreeSet<Integer>>();
@@ -66,21 +68,109 @@ public class QueryHandler {
                 termToPos.put(term, currEntry.getPositions());
             }
         }
-        
+
         // Ensure all the documents have all the terms. Removes the invalid documents.
         HashMap<String, HashMap<String, TreeSet<Integer>>> result =
-                new HashMap<String, HashMap<String, TreeSet<Integer>>>();
-        
+            new HashMap<String, HashMap<String, TreeSet<Integer>>>();
+
         for (String documentId : documents.keySet()) {
             if (documents.get(documentId).keySet().size() == query.getTerms().size()) {
                 result.put(documentId, documents.get(documentId));
                 // System.out.println(documentId);
             }
         }
-        
+
         return result;
     }
 
+
+
+    private boolean checkProximity(HashMap<String, ArrayList<Integer>> positions, Query query, int dist){
+        ArrayList<String> terms = query.getTerms();
+        HashMap<String, Integer> lastSeen = new HashMap<String, Integer>();
+        HashMap<String, Integer> lastIndexSeen = new HashMap<String, Integer>();
+        HashMap<String, Integer> nrSameTerm = new HashMap<String, Integer>();
+        HashMap<String, Iterator<Integer>> mapIt = new HashMap<String, Iterator<Integer>>();
+
+        for(String term : terms){
+            if(positions.containsKey(term) == false)
+                return false;    
+            if(nrSameTerm.containsKey(term)){
+                int nr = nrSameTerm.get(term) + 1;
+                nrSameTerm.put(term, nr);
+            }else{
+                nrSameTerm.put(term, 1);
+            }
+
+            Iterator<Integer> it = positions.get(term).iterator();
+            mapIt.put(term, it);
+        }
+
+
+        for(String term: mapIt.keySet()){
+            Iterator<Integer> it = mapIt.get(term);
+            if(it.hasNext()){
+                lastSeen.put(term, it.next());
+                lastIndexSeen.put(term, 0);
+            }
+        }
+
+        boolean flag = true;
+        while(flag){
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            String minTerm = null;
+            boolean allTermsMatched = true;
+
+            for(String term: lastSeen.keySet()){
+
+                int nrOcc = nrSameTerm.get(term);
+                if(nrOcc > 1 && lastIndexSeen.get(term) >= nrOcc - 1){
+                    ArrayList<Integer> currPos = positions.get(term);
+                    for(int i = lastIndexSeen.get(term); i >= 0; i-- ){
+                        if(min >currPos.get(i)){
+                            min = currPos.get(i);
+                            minTerm = term;  
+                        }
+                        if(max < currPos.get(i)){
+                            max = currPos.get(i);
+                        }
+                    }
+                }
+
+                if(nrOcc > 1 && lastIndexSeen.get(term) < nrOcc -1) 
+                    allTermsMatched = false;
+                if(nrOcc == 1){   
+                    int index = lastSeen.get(term);
+                    if(min > index){
+                        min = index;
+                        minTerm = term;
+                    }
+
+                    if(max  < index){
+                        max = index;
+                    }
+                }   
+            }
+
+            if(allTermsMatched == true){
+                if(max - min <= dist)
+                    return true;
+                else{
+                    Iterator<Integer> it = mapIt.get(minTerm);
+                    if(it.hasNext() == false)
+                        return false;
+                    int nextIndx = it.next();
+                    int last = lastIndexSeen.get(minTerm);
+                    last ++;
+                    lastIndexSeen.put(minTerm, last);
+                    lastSeen.put(minTerm, nextIndx);
+                }
+            }
+        }
+        
+        return false;
+    }
 
     /**
      * Answers proximity queries
@@ -91,11 +181,30 @@ public class QueryHandler {
     private ArrayList<String> handleProximityQuery(Query query) {
 
         int distance = query.getProximityWindow();    
-        HashMap<String, HashMap<String, TreeSet<Integer>>>
-        intersect = getDocIdsMatchingPhrase(query);
+        HashMap<String, HashMap<String, TreeSet<Integer>>> intersect =
+            getDocIdsMatchingPhrase(query);
+        ArrayList<String> matchingDocs = new ArrayList<String>();
+
+        // Filters the documents that contain the whole phrase.
+        for (String documentId : intersect.keySet()) {
+            HashMap<String, TreeSet<Integer>> termsPos = intersect.get(documentId);
+            HashMap<String, ArrayList<Integer>> termsPosList = 
+                new HashMap<String, ArrayList<Integer>>();
+
+            System.out.println("Document: " + documentId);
+            for(String term: termsPos.keySet()){
+                termsPosList.put(term, new ArrayList<Integer>(termsPos.get(term)));
+          //      for(Integer p : termsPos.get(term)){
+           //     System.out.println("Term: " + term + " " +termsPos.get(term).size());
+            //    }
+            }
+            if(checkProximity(termsPosList, query, distance)){
+                matchingDocs.add(documentId);
+            }
+        }            
 
 
-        return null;
+        return matchingDocs;
     }
 
     /**
@@ -106,18 +215,18 @@ public class QueryHandler {
      */
     private ArrayList<String> handlePhraseQuery(Query query) {
         HashMap<String, HashMap<String, TreeSet<Integer>>> commonDocumentEntries =
-                getDocIdsMatchingPhrase(query);
+            getDocIdsMatchingPhrase(query);
         ArrayList<String> matchingDocs = new ArrayList<String>();
-        
+
         // Filters the documents that contain the whole phrase.
         for (String documentId : commonDocumentEntries.keySet()) {
             TreeSet<Integer> positions =
-                    commonDocumentEntries.get(documentId).get(query.getTerm(0));
+                commonDocumentEntries.get(documentId).get(query.getTerm(0));
             // System.out.println("Term0: " + query.getTerm(0) + " " + positions.toString());
 
             for (int i = 1; i < query.getTerms().size(); i++) {
                 ArrayList<Integer> incrementedPositions = new ArrayList<Integer>();
-                
+
                 // Increment the positions from the previous term.
                 for (Iterator<Integer> it = positions.iterator(); it.hasNext(); ) {
                     incrementedPositions.add(it.next() + 1);
@@ -128,12 +237,12 @@ public class QueryHandler {
                 positions.retainAll(incrementedPositions);
                 // System.out.println("After Term" + i + ": " + query.getTerm(i) + " " + positions.toString());
             }
-            
+
             if (!positions.isEmpty()) {
                 matchingDocs.add(documentId);
             }
         }
-        
+
         return matchingDocs;
     }
 
