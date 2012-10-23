1b81ecfdad62982bf0b745aa0173a59e7c4bbe49
lvamanu <loredanavamanu@gmail.com>
2012-10-23 18:24:19 +0200

    nothing

--- src/PrecisionRecall.java
+++ src/PrecisionRecall.java
@@ -1,22 +1,12 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 
 public class PrecisionRecall {
@@ -115,34 +105,5 @@ public class PrecisionRecall {
 
         return avg;
 
-    }
-    
-    public void generateGraph() throws IOException{
-        XYSeriesCollection dataset = new XYSeriesCollection();  
-        XYSeries xySeries = new XYSeries( "Precision-Recall Graph");  
-        xySeries.add( 1, 10 );  
-        xySeries.add( 2, 4 );  
-        xySeries.add( 3, 6 );  
-        xySeries.add( 4, 12 );  
-        xySeries.add( 5, 11 );  
-        xySeries.add( 6, 29 );  
-        xySeries.add( 7, 33 );  
-       
-   
-       
-        dataset.addSeries( xySeries );  
-    
-       
-        JFreeChart chart = ChartFactory.createXYLineChart( "Park Chart",  
-                                                           "Day",  
-                                                           "# of Visitors",  
-                                                           dataset,  
-                                                           PlotOrientation.VERTICAL,  
-                                                           true,  
-                                                           false,  
-                                                           false );  
-       
-        FileOutputStream out = new FileOutputStream( new File( "parkchart.png" ) );  
-        ChartUtilities.writeChartAsPNG( out, chart, 400, 200 );    
     }
 }
