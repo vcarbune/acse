8f6252d2d3204f43f2f23a5f27403aeeaa003dd8
Victor Ungureanu <victoru89@gmail.com>
2012-11-19 18:26:07 +0100

    Created interface stubs for DocEntry and DocSet.

--- src/DocEntry.java
+++ src/DocEntry.java
@@ -1,29 +1,55 @@
 
+import java.util.HashMap;
+
 /**
  * Wrapper to easily represent and sort pairs of type (docId, frequency) within the DataSet.
  */
+// TODO(uvictor)
 public class DocEntry implements Comparable<DocEntry> {
 
+    private static class Word {
+
+        private int hamCount;
+        private int spamCount;
+
+        public int getHamCount() {
+            return hamCount;
+        }
+
+        public void incHamCount() {
+            this.hamCount++;
+        }
+
+        public int getSpamCount() {
+            return spamCount;
+        }
+
+        public void incSpamCount() {
+            this.spamCount++;
+        }
+    }
     private String docId;
-    private int frequency;
+    private HashMap<String, Word> wordCounts;
 
     public DocEntry(String docId) {
         this.docId = docId;
-        this.frequency = 1;
+        this.wordCounts = new HashMap<String, Word>();
     }
 
     public String getDocId() {
         return docId;
     }
 
-    public int getFrequency() {
-        return frequency;
+    public int getCount(String term) {
+        Word word = wordCounts.get(term);
+        //return (double) word.getSpamCount() / (word.getSpamCount() + word.get)
+        return 0;
     }
 
-    public void incFrequency() {
-        frequency++;
-    }
-
+    public void incCount(String word) {
+        // TODO(uvictor)
+    }   
+    
     @Override
     public int compareTo(DocEntry entry) {
         return docId.compareTo(entry.getDocId());
---
+++ src/DocSet.java
@@ -0,0 +1,23 @@
+
+// TODO(uvictor)
+public class DocSet {
+
+    public int getHamCount(final String docId) {
+        return 0;
+    }
+
+    public int getSpamCount(final String docId) {
+        return 0;
+    }
+
+    public void incCount(final String docId, final String word) {
+    }
+    
+    public void add(final DocSet docSet) {
+        
+    }
+    
+    public void substract(final DocSet docSet) {
+        
+    }
+}
