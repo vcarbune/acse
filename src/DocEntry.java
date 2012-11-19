51e1b0ab48bd6c646925956d93ed3a7dfa8ee784
lvamanu <loredanavamanu@gmail.com>
2012-11-20 00:07:52 +0100

    Added a null test in the incCount method.

--- src/DocEntry.java
+++ src/DocEntry.java
@@ -5,37 +5,41 @@ import java.util.Set;
 
 public class DocEntry {
 
-    private String docId;
-    private Boolean spam;
-    private HashMap<String, Integer> wordCounts;
+	private String docId;
+	private Boolean spam;
+	private HashMap<String, Integer> wordCounts;
 
-    public DocEntry(String docId, Boolean spam) {
-        this.docId = docId;
-        this.spam = spam;
-        this.wordCounts = new HashMap<String, Integer>();
-    }
+	public DocEntry(String docId, Boolean spam) {
+		this.docId = docId;
+		this.spam = spam;
+		this.wordCounts = new HashMap<String, Integer>();
+	}
 
-    public String getDocId() {
-        return docId;
-    }
-    
-    /**
-     * @throws NullPointerException if we don't know yet if document is spam or not
-     */
-    public boolean isSpam() {
-        return spam;
-    }
+	public String getDocId() {
+		return docId;
+	}
 
-    public int getCount(String term) {
-        return wordCounts.get(term);
-    }
+	/**
+	 * @throws NullPointerException if we don't know yet if document is spam or not
+	 */
+	public boolean isSpam() {
+		return spam;
+	}
 
-    public void incCount(String word) {
-        int count = wordCounts.get(word);
-        wordCounts.put(word, count + 1);
-    }
-    
-    public Set<Map.Entry<String, Integer>> getWordCounts() {
-        return wordCounts.entrySet();
-    }
+	public int getCount(String term) {
+		return wordCounts.get(term);
+	}
+
+	public void incCount(String word) {
+		if(wordCounts.get(word) == null)
+			wordCounts.put(word, 1);
+		else{
+			int count = wordCounts.get(word);
+			wordCounts.put(word, count + 1);
+		}
+	}
+
+	public Set<Map.Entry<String, Integer>> getWordCounts() {
+		return wordCounts.entrySet();
+	}
 }
