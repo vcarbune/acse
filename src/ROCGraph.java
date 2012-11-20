2d4817d36df74b4d479481d986c424bf0476ac32
lvamanu <loredanavamanu@gmail.com>
2012-11-20 23:27:20 +0100

    Added labels on points.

--- src/ROCGraph.java
+++ src/ROCGraph.java
@@ -1,15 +1,24 @@
 import java.awt.Color;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.Serializable;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.LegendItem;
+import org.jfree.chart.LegendItemCollection;
+import org.jfree.chart.labels.StandardXYItemLabelGenerator;
+import org.jfree.chart.labels.XYItemLabelGenerator;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.PublicCloneable;
 
 
 public class ROCGraph {
@@ -18,19 +27,22 @@ public class ROCGraph {
 	private ArrayList<Double> tpRate = new ArrayList<Double>();
 
 	public void addPoint(double tp, double fp) {
-	    tpRate.add(tp);
-	    fpRate.add(fp);
+		tpRate.add(tp);
+		fpRate.add(fp);
 	}
-	
+
 	public void createROCGraph(String fileName){
 		XYSeries series = new XYSeries("ROC");
+		StringBuilder builder = new StringBuilder();
 		int poz = 0;
 		for(double fp : fpRate){
 			double tp = tpRate.get(poz++);
 			series.add(fp, tp);
+			builder.append(poz).append(" ");
 		}
 
 		XYDataset xyDataset = new XYSeriesCollection(series);
+
 		final JFreeChart chart = ChartFactory.createXYLineChart(
 				"ROC Curve",      // chart title
 				"False positive rate",                      // x axis label
@@ -42,18 +54,40 @@ public class ROCGraph {
 				false                     // urls
 		);
 
-		chart.setBackgroundPaint(Color.white);
-		
-		
+		chart.setBackgroundPaint(Color.WHITE);
+
+		XYLineAndShapeRenderer renderer =
+			new XYLineAndShapeRenderer(true, true);
+		renderer.setBaseShapesVisible(true);
+		renderer.setBaseShapesFilled(true);
+		((XYPlot)chart.getPlot()).setRenderer(renderer);
+		// label the points
+		NumberFormat format = NumberFormat.getNumberInstance();
+		format.setMaximumFractionDigits(2);
+		XYItemLabelGenerator generator = new MyGenerator();
+		renderer.setBaseItemLabelGenerator(generator);
+		renderer.setBaseItemLabelsVisible(true);
+
+
 		// Creating image file for the chart
 		try {
-		    FileOutputStream out = new FileOutputStream(new File(fileName));
-		    ChartUtilities.writeChartAsPNG(out, chart, 800, 600);
- 
-            out.close();
+			FileOutputStream out = new FileOutputStream(new File(fileName));
+			ChartUtilities.writeChartAsPNG(out, chart, 800, 600);
+
+			out.close();
 		}
 		catch (Exception e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
+}
+
+class MyGenerator implements XYItemLabelGenerator {
+
+	private static int poz = 0;
+    @Override
+    public String generateLabel(XYDataset dataset, int series, int item) {
+         poz++;
+         return "" + poz;
+    }
 }
