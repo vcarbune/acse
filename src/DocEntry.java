54bec8addfeae9a9887c56c2b4645487df6e1135
Mircea Grecu <mircea@MG-Mac.local>
2012-11-25 16:32:38 +0100

    Phase 2: added precomputation of tf-idf values in DocEntry.

--- src/DocEntry.java
+++ src/DocEntry.java
@@ -8,6 +8,8 @@ public class DocEntry {
     private String docId;
     private Boolean spam;
     private HashMap<String, Integer> wordCounts;
+    private HashMap<String, Double> wordWeights= null; // tf-idf weights
+    private double vectorLength;
 
     public DocEntry(String docId, Boolean spam) {
         this.docId = docId;
@@ -31,14 +33,17 @@ public class DocEntry {
     }
 
     public void incCount(String word) {
-
-		if(wordCounts.get(word) == null)
-			wordCounts.put(word, 1);
-		else{
-			int count = wordCounts.get(word);
-			wordCounts.put(word, count + 1);
-		}
+        incCount(word, 1);
 	}
+    
+    public void incCount(String word, int inc) {
+        if(wordCounts.get(word) == null)
+            wordCounts.put(word, inc);
+        else{
+            int count = wordCounts.get(word);
+            wordCounts.put(word, count + inc);
+        }
+    }
     
     public Set<Map.Entry<String, Integer>> getWordCounts() {
         return wordCounts.entrySet();
@@ -46,5 +51,61 @@ public class DocEntry {
     
     public Set<String> getWords() {
         return wordCounts.keySet();
+    }
+    
+    /**
+     * For phase 2.
+     *  
+     * Computes the map with the tf-idf frequencies for each word in the doc,
+     * using a map tha contains the df values for each term.
+     * 
+     * @param docFrequencyMap df values for each term
+     * @param numDocs N in the tf-idf formula
+     */
+    public void computeWordWeights(HashMap<String, Integer> docFrequencyMap, int numDocs) {
+        
+        if (wordCounts.size() == 0) {
+            throw new RuntimeException("The wordCounts map is empty!");
+        }
+        
+        wordWeights = new HashMap<String, Double>();
+        vectorLength = 0;
+        
+        for (String term: wordCounts.keySet()) {
+            double tf_idf = (1 + Math.log10(wordCounts.get(term))) * 
+                    Math.log10(numDocs / (double) docFrequencyMap.get(term));
+            wordWeights.put(term, tf_idf);
+            vectorLength += tf_idf * tf_idf;
+        }
+        
+        vectorLength = Math.sqrt(vectorLength);
+    }
+    
+    /**
+     * For phase 2.
+     * 
+     * Computes the distance between 2 documents, 
+     * 
+     * @param other
+     * @return
+     */
+    public double getDistance(DocEntry other) {
+        //TODO: which formula? cosine doesn't seem suitable here...
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+
+        // Testing computeWordWeights
+        DocEntry doc = new DocEntry("doc1", true);
+        doc.incCount("asd", 100);
+        doc.incCount("qwe", 10);
+        HashMap<String, Integer> dfMap = new HashMap<String, Integer>();
+        dfMap.put("asd", 10);
+        dfMap.put("qwe", 1);
+        doc.computeWordWeights(dfMap, 100);
+        
+        System.out.println(doc.wordWeights);
+        System.out.println(doc.vectorLength);
     }
 }
