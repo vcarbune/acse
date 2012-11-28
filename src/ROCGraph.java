e5bed89f9c1b6bd5b534ccaa28e38fa54bc29460
Mircea Grecu <mircea@MG-Mac.local>
2012-11-28 08:48:03 +0100

    Small change to ROC curve.

--- src/ROCGraph.java
+++ src/ROCGraph.java
@@ -69,6 +69,9 @@ public class ROCGraph {
 		XYItemLabelGenerator generator = new MyGenerator();
 		renderer.setBaseItemLabelGenerator(generator);
 		renderer.setBaseItemLabelsVisible(true);
+		
+		
+		chart.getXYPlot().getDomainAxis().setLowerBound(-0.05);
 
 
 		// Creating image file for the chart
